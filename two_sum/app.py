c7c251b8f68ac9176303fefef7e69e89c8ef66f5
dolsen0 <derek.olsen@gmail.com>
2023-08-11 16:43:20 -0400

    added two_sum with 0(n) space and time complexity

---
+++ two_sum/app.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def two_sum(nums, target):
+    dict = {}
+    for i in range(len(nums)):
+        difference = target - nums[i]
+        if difference in dict:
+            return [dict[difference], i]
+        else:
+            dict[nums[i]] = i
+print(two_sum([2,3,9,7,11,15], 9))  # [0, 1]
+print(two_sum([3,2,4], 6))  # [1, 2]
+print(two_sum([2,3,7,1,6,2], 9))    # [2, 4]
+
+# Function that takes in an array of integers and a target integer
+# time: O(n)
+# space: O(n)
+
